533b2feed196f4f7e34ecdd94c2e2d55d9a21adb
lixiaojing <lixiaojing@etaostars.com>
2016-01-11 15:30:32 +0800

    mvc初始化

---
+++ Easy.Register/App_Start/FilterConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Easy.Register
+{
+    /// <summary>
+    /// MVC过滤器配置
+    /// </summary>
+    public class FilterConfig
+    {
+        /// <summary>
+        /// 注册过滤器
+        /// </summary>
+        /// <param name="filters"></param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ErrorAttribute());
+        }
+    }
+}
---
+++ Easy.Register/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Easy.Register.Global" Language="C#" %>
