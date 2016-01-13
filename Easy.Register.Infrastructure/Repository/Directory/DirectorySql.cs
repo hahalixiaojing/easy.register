577948a7c80ce365f6c4e8ab405c0cf89f6e567c
lixiaojing <lixiaojing@etaostars.com>
2016-01-13 15:23:55 +0800

    提交文件

--- Easy.Register.Infrastructure/Repository/Directory/DirectorySql.cs
+++ Easy.Register.Infrastructure/Repository/Directory/DirectorySql.cs
@@ -8,6 +8,21 @@ namespace Easy.Register.Infrastructure.Repository.Directory
 {
     static class DirectorySql
     {
+        public static Tuple<string, dynamic> Add(Model.Directory directory)
+        {
+            const string sql = @"INSERT INTO regisrer_directory
+	                            (name, description, ping_api_path, version_api_path, create_date)
+	                            VALUES (@Name, @Desc, @PingApiPath, @VersionApiPath, @Date);SELECT last_insert_id()";
 
+
+            return new Tuple<string, dynamic>(sql, new
+            {
+                Name = directory.Name,
+                Desc = directory.Description,
+                PingApiPath = directory.PingAPIPath,
+                VersionApiPath = directory.VersionAPIPath,
+                Date = directory.CreateDate
+            });
+        }
     }
 }
