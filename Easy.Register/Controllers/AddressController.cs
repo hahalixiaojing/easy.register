33520eda18660217ba08278ff5a9198e0b65e855
lixiaojing <lixiaojing@etaostars.com>
2016-01-19 17:11:39 +0800

    提交文件

--- Easy.Register/Controllers/AddressController.cs
+++ Easy.Register/Controllers/AddressController.cs
@@ -9,19 +9,31 @@ namespace Easy.Register.Controllers
     public class AddressController : Controller
     {
         /// <summary>
+        /// 消费者和提供者关系注册
+        /// </summary>
+        /// <param name="consumerDirectoryName"></param>
+        /// <param name="providerDirectoryName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddRelation(string consumerDirectoryName,string[] providerDirectoryName)
+        {
+            //TODO:注册关系时，先要删除关系再添加
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// 拉取指定目录的Node节点地址
         /// </summary>
         /// <param name="directory">提供者目录名称</param>
-        /// <param name="consumerDirectory">消费者目录名称</param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Pull(string providerDirectory, string consumerDirectory)
+        public ActionResult Pull(string providerDirectory)
         {
             //在拉取提供者地址信息的时候，需要触发一个事件，注册提供者和消费者提供之间的关系
             throw new NotImplementedException();
         }
         /// <summary>
-        /// 注册指定目录节点地址
+        /// 节点注册
         /// </summary>
         /// <param name="directory">目录名称</param>
         /// <param name="url">API地址</param>
@@ -31,15 +43,10 @@ namespace Easy.Register.Controllers
         /// <param name="status">状态</param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult ProviderRegister(string directory, string url, string ip, string desc, int weight, int status)
+        public ActionResult NodeRegister(string directory, string url, string ip, string desc, int weight, int status)
         {
             //注册时，如果没有对应的directory，则直接注册成消费者，如果有则只注册Node
             //Directory之间需要建立关系
-            throw new NotImplementedException();
-        }
-        [HttpPost]
-        public ActionResult ConsumerRegister(string directory, string url, string ip, string desc, int weight, int status)
-        {
             throw new NotImplementedException();
         }
     }
