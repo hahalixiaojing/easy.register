95901c9bc79e3d5fb0095c1a25d44d5ef75b59c5
lixiaojing <lixiaojing@etaostars.com>
2016-01-20 16:56:37 +0800

    提交文件

---
+++ Easy.Register.Model/PublishService/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy.Register.Model.PublishService
+{
+    public class Node
+    {
+        public Node(string providerName, string url,int weight,bool isAvaiable)
+        {
+            this.ProviderName = providerName;
+            this.Url = url;
+            this.Weight = weight;
+            this.IsAvailable = isAvaiable;
+        }
+        public String ProviderName
+        {
+            get;
+            private set;
+        }
+        public String Url
+        {
+            get;
+            private set;
+        }
+        public Int32 Weight
+        {
+            get;
+            private set;
+        }
+        public Boolean IsAvailable
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ Easy.Register.Model/ServiceRegistry.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Reflection;
+using Easy.Domain.ServiceFramework;
+using Easy.Register.Model.PublishService;
+
+namespace Easy.Register.Model
+{
+    public static class ServiceRegistry
+    {
+        readonly static ServiceFactory factory;
+
+        static ServiceRegistry()
+        {
+            ServiceFactoryBuilder b = new ServiceFactoryBuilder();
+            Stream stream = Assembly.ReflectionOnlyLoadFrom("Easy.Register.Infrastructure.dll").GetManifestResourceStream("Easy.Register.Infrastructure.Service.service.xml");
+
+            factory = b.Build(stream);
+        }
+
+        public static IPublishService PublishService
+        {
+            get
+            {
+                return factory.Get<IPublishService>();
+            }
+        }
+    }
+}
