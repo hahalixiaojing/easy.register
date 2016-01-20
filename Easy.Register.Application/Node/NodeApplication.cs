b87d9761af3bcd4b249b6f8f375208984fafaec4
lixiaojing <lixiaojing@etaostars.com>
2016-01-20 14:35:49 +0800

    更新easy.domain

---
+++ Easy.Register.Application/Node/NodeApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Easy.Domain.Application;
+
+namespace Easy.Register.Application.Node
+{
+    public class NodeApplication : BaseApplication
+    {
+
+    }
+}
