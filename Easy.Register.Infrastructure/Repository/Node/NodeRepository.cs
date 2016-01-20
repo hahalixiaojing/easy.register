8c1c58aa8a53ddc5be9f91b06e278c3e2f593aed
li.kun <williamnet_li@yahoo.com>
2016-01-20 09:39:23 +0800

    node实例化

--- Easy.Register.Infrastructure/Repository/Node/NodeRepository.cs
+++ Easy.Register.Infrastructure/Repository/Node/NodeRepository.cs
@@ -2,39 +2,72 @@
 using System.Collections.Generic;
 using Easy.Domain.RepositoryFramework;
 using Easy.Register.Model;
+using Dapper;
+using System.Linq;
 
 namespace Easy.Register.Infrastructure.Repository.Node
 {
     public class NodeRepository : INodeRepository, IDao
     {
+        private static Easy.Public.EntityPropertyHelper<Model.Node> propertyHelper = new Public.EntityPropertyHelper<Model.Node>();
+
         public void Add(Model.Node item)
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                var tuple = NodeSql.Add(item);
+
+                int id = conn.ExecuteScalar<int>(tuple.Item1, (object)tuple.Item2);
+                propertyHelper.SetValue<int>(m => m.Id, item, id);
+            }
         }
 
         public IList<Model.Node> FindAll()
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                return conn.Query<Model.Node>(NodeSql.FindAll()).ToList();
+            }
         }
 
         public Model.Node FindBy(int key)
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                return conn.Query<Model.Node>(NodeSql.FindById(key)).FirstOrDefault();
+            }
         }
 
         public bool IsExists(Model.Node n)
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                var tuple = NodeSql.IsExists(n.DirectoryInfo.Id, n.Url);
+
+                var model = conn.Query<Model.Node>(tuple.Item1, (object)tuple.Item2);
+
+                if (model.Any())
+                {
+                    return false;
+                }
+                return true;
+            }
         }
 
         public void Remove(Model.Node item)
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                conn.Execute(NodeSql.Remove(item.Id));
+            }
         }
 
         public void RemoveAll()
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                conn.Execute(NodeSql.RemoveAll());
+            }
         }
 
         public IEnumerable<Model.Node> Select(int directoryId)
@@ -44,7 +77,12 @@ namespace Easy.Register.Infrastructure.Repository.Node
 
         public void Update(Model.Node item)
         {
-            throw new NotImplementedException();
+            using (var conn = Database.Open())
+            {
+                var tuple = NodeSql.Update(item);
+
+                conn.ExecuteScalar(tuple.Item1, (object)tuple.Item2);
+            }
         }
     }
 }
--- Easy.Register.Infrastructure/Repository/Node/NodeSql.cs
+++ Easy.Register.Infrastructure/Repository/Node/NodeSql.cs
@@ -8,6 +8,90 @@ namespace Easy.Register.Infrastructure.Repository.Node
 {
     static class NodeSql
     {
+        static string BaseSelectSql()
+        {
+            return @"SELECT id, url, ip, description, weight, `status`, create_date, directory_id, directory_name
+	FROM register_node";
+        }
 
+        public static Tuple<string, dynamic> Add(Model.Node item)
+        {
+            const string sql = @"INSERT INTO register_node
+	(url, ip, description, weight, `status`, create_date, directory_id, directory_name)
+	VALUES (@url, @ip, @description, @weight, @status, @create_date, @directory_id, @directory_name);SELECT last_insert_id()";
+
+
+            return new Tuple<string, dynamic>(sql, new
+            {
+                url = item.Url,
+                ip = item.Ip,
+                description = item.Description,
+                weight = item.Weight,
+                status = item.Status,
+                create_date = item.CreateDate,
+                directory_id = item.DirectoryInfo.Id,
+                directory_name = item.DirectoryInfo.Name
+            });
+        }
+
+        public static Tuple<string, dynamic> Update(Model.Node item)
+        {
+            string sql = @"UPDATE register_node
+	                SET
+		                url=@url,
+		                ip=@ip,
+		                description=@description,
+		                weight=@weight,
+		                `status`=@status,
+		                create_date=@create_date,
+		                directory_id=@directory_id,
+		                directory_name=@directory_name
+                    WHERE id = @Id";
+
+
+            return new Tuple<string, dynamic>(sql, new
+            {
+                Id = item.Id,
+                url = item.Url,
+                ip = item.Ip,
+                description = item.Description,
+                weight = item.Weight,
+                status = item.Status,
+                create_date = item.CreateDate,
+                directory_id = item.DirectoryInfo.Id,
+                directory_name = item.DirectoryInfo.Name
+            });
+        }
+
+        public static string RemoveAll()
+        {
+            return "delete from register_node";
+        }
+
+        public static string Remove(int id)
+        {
+            return string.Join(" ", RemoveAll(), "WHERE id=" + id);
+        }
+
+        public static string FindById(int id)
+        {
+            string sql = string.Join(" ", BaseSelectSql(), "WHERE", "id=" + id);
+            return sql;
+        }
+
+        public static string FindAll()
+        {
+            return BaseSelectSql();
+        }
+
+        public static Tuple<string, dynamic> IsExists(int directiory_id, string url)
+        {
+            string sql = string.Join(" ", BaseSelectSql(), "where directory_id=@directory_id and url=@url");
+            return new Tuple<string, dynamic>(sql, new
+            {
+                directory_id = directiory_id,
+                url = url
+            });
+        }
     }
 }
