575b17a2068dfeafe8168d38d159e1a77d7c4fd9
yangli_sky@163.com <yangli_sky@163.com>
2016-01-26 11:11:12 +0800

    a

--- Easy.Register/Controllers/ConsumerController.cs
+++ Easy.Register/Controllers/ConsumerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Easy.Register.Application;
+using Easy.Register.Application.Models.Node;
 
 namespace Easy.Register.Controllers
 {
@@ -11,6 +13,10 @@ namespace Easy.Register.Controllers
         // GET: Consumer
         public ActionResult Index()
         {
+            IList<Node> list= ApplicationRegistry.Node.SelectByDirectoryType(1);
+
+            ViewBag.list = list;
+
             ViewBag.Active = "Cus";
             return View();
         }
