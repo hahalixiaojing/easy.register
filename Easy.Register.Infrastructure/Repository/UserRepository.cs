e5f68b5555361c1bc680814e21daf66ba1629207
lixiaojing <lixiaojing@etaostars.com>
2016-01-29 15:37:40 +0800

    提交文件

---
+++ Easy.Register.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Easy.Register.Model.User;
+
+namespace Easy.Register.Infrastructure.Repository.User
+{
+    public class UserRepository : IUserRepository
+    {
+        public void Add(Model.User.User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Model.User.User> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.User.User FindBy(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Model.User.User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Model.User.User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UsernameIsExists(string usrname, int currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Easy.Register.Model/User/UserValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Easy.Domain.Validators;
+
+namespace Easy.Register.Model.User
+{
+    public class UserValidation : EntityValidation<User>
+    {
+        public UserValidation()
+        {
+            this.IsNullOrWhiteSpace(m => m.Username, "");
+            this.IsNullOrWhiteSpace(m => m.Name, "");
+            this.IsNullOrWhiteSpace(m => m.Password, "");
+        }
+    }
+}
