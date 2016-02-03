703777ef786f2429d7acfcd7c49970827b936871
lixiaojing <lixiaojing@etaostars.com>
2016-02-03 16:15:18 +0800

    提交

--- Easy.Register.Application/Node/DoubleWeightDomainEvents/DecreaseWeightNodeSubscriber.cs
+++ Easy.Register.Application/Node/DoubleWeightDomainEvents/DecreaseWeightNodeSubscriber.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Easy.Register.Application.Node.DoubleWeight
+namespace Easy.Register.Application.Node.DoubleWeightDomainEvents
 {
     public class DecreaseWeightNodeSubscriber : NodePublishSubscriber
     {
