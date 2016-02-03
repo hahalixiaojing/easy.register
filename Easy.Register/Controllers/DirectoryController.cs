bf7be7c0f6520e05cb57790388160e57fa47f847
lixiaojing <lixiaojing@etaostars.com>
2016-02-03 16:31:16 +0800

    提交文件

--- Easy.Register/Controllers/DirectoryController.cs
+++ Easy.Register/Controllers/DirectoryController.cs
@@ -20,13 +20,11 @@ namespace Easy.Register.Controllers
 
             return View(model as IEnumerable<DirectoryModel>);
         }
-
         public ActionResult Add()
         {
             ViewBag.Active = "Dir";
             return View();
         }
-
         [HttpPost]
         public ActionResult AddPost(string name, string ip, string path, string type, string content)
         {
@@ -40,55 +38,6 @@ namespace Easy.Register.Controllers
             }
             ViewBag.Ok = model;
             return View();
-        }
-
-
-        [HttpPost]
-        public void updatePingAPIPath(int id, string PingAPIPath)
-        {
-            var r = ApplicationRegistry.Directory.FindById(id);
-            if (r == null)
-            {
-                return;
-            }
-
-            ApplicationRegistry.Directory.Update(id, r.Description, PingAPIPath, r.VersionAPIPath, r.DirectoryType);
-        }
-
-        [HttpPost]
-        public void updateVersionAPIPath(int id, string VersionAPIPath)
-        {
-            var r = ApplicationRegistry.Directory.FindById(id);
-            if (r == null)
-            {
-                return;
-            }
-
-            ApplicationRegistry.Directory.Update(id, r.Description, r.PingAPIPath, VersionAPIPath, r.DirectoryType);
-        }
-
-        [HttpPost]
-        public void updateType(int id, int Type)
-        {
-            var r = ApplicationRegistry.Directory.FindById(id);
-            if (r == null)
-            {
-                return;
-            }
-
-            ApplicationRegistry.Directory.Update(id, r.Description, r.PingAPIPath, r.VersionAPIPath, Type);
-        }
-
-        [HttpPost]
-        public void updateDescription(int id, string Description)
-        {
-            var r = ApplicationRegistry.Directory.FindById(id);
-            if (r == null)
-            {
-                return;
-            }
-
-            ApplicationRegistry.Directory.Update(id, Description, r.PingAPIPath, r.VersionAPIPath, r.DirectoryType);
         }
     }
 }
